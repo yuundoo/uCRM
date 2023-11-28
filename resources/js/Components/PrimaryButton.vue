d66aa0fb7046e3c83146ee7392491315791da980
yoondu <tyi020382@naver.com>
2023-11-28 21:29:33 +0900

    スタイル整理

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,17 +1,17 @@
 <script setup>
 defineProps({
-    type: {
-        type: String,
-        default: 'submit',
-    },
+   type: {
+      type: String,
+      default: 'submit',
+   },
 });
 </script>
 
 <template>
-    <button
-        :type="type"
-        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-    >
-        <slot />
-    </button>
+   <button
+      :type="type"
+      class="inline-flex items-center px-4 py-2 mb-3 mr-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+   >
+      <slot />
+   </button>
 </template>
