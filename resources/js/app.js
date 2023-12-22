9395d52f217d69022e53e29195c8913bcda37922
d-yoon <d-yoon@advanced-media.co.jp>
2023-12-22 11:34:02 +0900

    amplify error update

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,9 @@ import '../css/micromodal.css'
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Ziggy } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -17,7 +16,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .use(Ziggy, Ziggy)
             .mount(el);
     },
     progress: {
